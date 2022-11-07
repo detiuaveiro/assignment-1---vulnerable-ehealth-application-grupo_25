0906976fb94a1e9efa105becc81bbe46c314334b
André Butuc <andrebutuc@ua.pt>
2022-11-07 12:46:45 +0000

    help

---
+++ project/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/logged')
+def logged():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
